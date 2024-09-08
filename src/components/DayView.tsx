59fd2bd294cb7d6a857c42b8f5e8591fdc23a6ac
SAR-dev <sayedarrafi.bd@gmail.com>
2024-09-09 03:03:40 +0900

    up

--- src/components/DayView.tsx
+++ src/components/DayView.tsx
@@ -2,9 +2,9 @@ import { timeRanges, months, getWeekday } from "../helpers/calendar"
 
 const DayView = ({ year, month, day }: { year: number, month: number, day: number }) => {
     return (
-        <div className="flex flex-col relative">
-            <div className="w-full flex border border-base-300 text-sm font-medium sticky top-0">
-                <div className="w-32 flex-shrink-0 h-8 flex justify-center items-center w-full bg-base-100 border-r border-base-300"></div>
+        <div className="flex flex-col divide-y divide-base-300 border border-base-300 relative">
+            <div className="w-full flex border-x border-base-300 text-sm font-medium sticky top-0">
+                <div className="w-32 flex-shrink-0 flex justify-center items-center w-full bg-base-100 border-r border-base-300"></div>
                 <div className="w-full">
                     <div className="py-2 flex items-center justify-between px-3">
                         <div>{day} {months.find(e => e.index == month)?.longName}, {year}</div>
@@ -13,7 +13,7 @@ const DayView = ({ year, month, day }: { year: number, month: number, day: numbe
                 </div>
             </div>
             {timeRanges.map((timeRange, i) => (
-                <div className="w-full flex border border-base-300 text-sm font-medium" key={i}>
+                <div className="w-full flex border-x border-base-300 text-sm font-medium" key={i}>
                     <div className="w-32 h-auto py-2 flex justify-center items-center w-full bg-base-100 border-r border-base-300">
                         {timeRange.start} - {timeRange.end}
                     </div>
