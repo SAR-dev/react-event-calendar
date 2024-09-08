a5a2049204a7a67ea0adf0bfd0a2cb106750d055
SAR-dev <sayedarrafi.bd@gmail.com>
2024-09-08 16:07:45 +0900

    daisy ui added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { getMonths, getWeekdays } from "./constants/calendar"
+import { CalendarViewTypes } from "./types"
 
 function App() {
   const weekdays = getWeekdays()
@@ -7,16 +8,24 @@ function App() {
 
   const [year, setYear] = useState(2024)
   const [month, setMonth] = useState(0)
+  const [view, setView] = useState<CalendarViewTypes>(CalendarViewTypes.MONTH)
 
   return (
     <div className="w-full h-screen p-5">
-      <div className="w-full flex justify-center mb-3">
-        {months[month].longName} {year}
+      <div className="w-full flex justify-between items-center mb-3">
+        <div>
+          {months[month].longName} {year}
+        </div>
+        <div className="join join-horizontal">
+          <button className="btn btn-sm join-item">Day</button>
+          <button className="btn btn-sm join-item">Week</button>
+          <button className="btn btn-sm join-item">Month</button>
+        </div>
       </div>
       <div className="flex flex-col">
-        <div className="w-full grid grid-cols-7 border border-gray-300 text-sm font-medium">
+        <div className="w-full grid grid-cols-7 border border-base-300 text-sm font-medium">
           {getWeekdays().map((weekday, i) => (
-            <div className="h-8 flex justify-center items-center w-full bg-gray-100 border-r border-gray-300" key={i}>
+            <div className="h-8 flex justify-center items-center w-full bg-base-100 border-r border-base-300" key={i}>
               {weekday.longName}
             </div>
           ))}
